7e0ed1b9b23f5b1117c7994f59bcb3951f119cbc
Manish Kumar <95469979+manish-tech-git@users.noreply.github.com>
2022-07-01 11:08:11 +0530

    Create Check Identical Trees.cpp

---
+++ Check Identical Trees.cpp
@@ -0,0 +1,38 @@
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+		
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+***********************************************************/
+
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+    // Write your code here. 	
+    
+    if(root1 == NULL && root2 == NULL) return true;
+    
+    if(root1 == NULL || root2 == NULL) return false;
+    
+    return ((root1 -> data == root2 ->  data ) && identicalTrees(root1 -> left , root2 -> left) && identicalTrees(root2 -> right, root1 -> right));
+    
+    
+    
+    
+}
+
+//Time Complexity - O(N)
+
+// Space Complexity - O(N)
