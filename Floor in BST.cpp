fbf32c84f333210719b8dbaf2b095305e0125f6a
Manish Kumar <95469979+manish-tech-git@users.noreply.github.com>
2022-07-02 14:41:35 +0530

    Create Floor in BST.cpp

---
+++ Floor in BST.cpp
@@ -0,0 +1,42 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int floorInBST(TreeNode<int> * root, int X)
+{
+    int floor = -1;
+    while(root) {
+        
+    if(root -> val == X){
+         floor = root -> val;
+         return floor;
+    }
+        
+    else if(X > root -> val){
+        floor = root -> val;
+        root = root -> right;
+    }
+    else 
+        root = root -> left;
+        
+    }
+    
+    return floor;
+}
+
