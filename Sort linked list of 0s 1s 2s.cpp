46d48892dbd5d32c12094fb4205fff944e45c07b
Manish Kumar <95469979+manish-tech-git@users.noreply.github.com>
2022-07-07 10:52:40 +0530

    Update Sort linked list of 0s 1s 2s.cpp

--- Sort linked list of 0s 1s 2s.cpp
+++ Sort linked list of 0s 1s 2s.cpp
@@ -54,3 +54,101 @@ Node* sortList(Node *head)
     
     return head;
  }
+
+
+
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+
+
+
+// Approach 2  - when the nodes replacement of nodes is not allowed
+
+/********************************
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+********************************/
+
+void insertAtTail (Node* &tail, Node* curr)
+{
+    tail -> next = curr;
+    tail = curr;
+    
+    
+}
+
+Node* sortList(Node *head)
+{
+    Node* zeroHead = new Node(-1);
+    Node* zeroTail = zeroHead;
+    
+    Node* oneHead = new Node(-1);
+    Node* oneTail = oneHead;
+    
+    Node* twoHead = new Node(-1);
+    Node* twoTail = twoHead;
+    
+    Node* curr = head;
+    
+    //Create seperate list 
+    while (curr != NULL)
+    {
+        
+        int value = curr -> data;
+        
+        if(value == 0){
+            insertAtTail(zeroTail, curr);
+        }
+        
+        else if (value == 1){
+            insertAtTail (oneTail, curr);
+        }
+        else{
+            insertAtTail (twoTail, curr);
+        }
+        
+        curr = curr -> next;
+    }
+    
+    //Merge Linked List
+    
+    if(oneHead -> next != NULL){
+        zeroTail -> next = oneHead -> next;
+    }
+    else{
+        //1s list
+        zeroTail -> next = twoHead -> next;
+        
+    }
+    oneTail -> next =   twoHead -> next;
+    twoTail -> next = NULL;
+    
+    //setup head
+    head = zeroHead ->next;
+    
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+    
+    return head;
+
+}
+
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+
+
