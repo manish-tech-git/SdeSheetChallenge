dc1b779f7d303419bb5e5b3a3634a6de55f72661
Manish Kumar <95469979+manish-tech-git@users.noreply.github.com>
2022-07-13 08:16:48 +0530

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -1,3 +1,5 @@
+//With using set (or using extra space)
+
 #include<bits/stdc++.h> 
 
 int removeDuplicates(vector<int> &arr, int n) {
@@ -11,3 +13,33 @@ int removeDuplicates(vector<int> &arr, int n) {
     return s.size();
     
 }
+
+// Time COmplexity - O(N)
+// Space Complexity - O(N)
+
+
+//******************************* Without using extra Space ***********************************************
+
+#include<bits/stdc++.h> 
+
+int removeDuplicates(vector<int> &arr, int n) {
+	
+  int  i = 0;
+  int j = 1;
+    int size = n;
+    
+    while (i < n-1 && j < n)
+    {
+        if (arr[i] == arr[j]){
+            size -= 1; 
+        }
+        i++;
+        j++;
+    }
+    return size;
+    
+}
+
+// Time COmplexity - O(N)
+// Space Complexity - O(1)
+
