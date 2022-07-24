6e5acd729468f3cfa7b14e57a3071daa5e14af0c
Manish Kumar <95469979+manish-tech-git@users.noreply.github.com>
2022-07-24 17:38:44 +0530

    Update Rat In A Maze.cpp

--- Rat In A Maze.cpp
+++ Rat In A Maze.cpp
@@ -1,4 +1,4 @@
-
+// *************************************** string version **********************************************
 
 #include<bits/stdc++.h>
 bool isSafe (int newx, int newy, vector <vector <bool >>&vis, vector < vector < int >> & arr, int n ){
@@ -65,3 +65,65 @@ vector < string > searchMaze(vector < vector < int >> & arr, int n) {
 //Time Complexity - (4 ^ n^2)
 // Space Complexity = O(N * M)
 
+
+
+// ***************************************************** integer version ***********************************************
+
+
+
+void putData (vector <vector <int>> &rat, vector <vector <int>> &ans)
+{
+    int n= rat.size();
+    vector <int> temp;
+    for (int i = 0; i  < n; i++){
+        for (int j = 0; j < n; j++){
+            temp.push_back(rat[i][j]);
+        }
+    }
+    ans.push_back(temp);
+}
+
+
+void solve (int row, int col, vector<vector<int> > &maze, vector<vector<int> > &rat, vector<vector<int> > &ans, vector<vector<bool> > &visited, int n)
+{
+    if (row >= n or row <0 or col >= n or col < 0 or visited[row][col] or maze[row][col] == 0)
+        return;
+    
+    if (row == n-1 and col == n-1){
+        rat[row][col] = 1;
+        putData(rat, ans);
+        rat[row][col] = 0;
+        return;
+    }
+    
+    visited[row][col] = 1;
+    rat[row][col] = 1;
+    
+    //DOWN
+    solve (row+1, col, maze, rat, ans, visited, n);
+    
+    //RIGHT
+    solve (row, col+1, maze, rat, ans, visited, n);
+    
+    //Up
+    solve (row-1, col, maze, rat, ans, visited, n);
+    
+    
+   //LEFT
+    solve (row, col-1, maze, rat, ans, visited, n);
+    
+    visited[row][col] = false;
+    rat[row][col] = 0;
+       
+}
+
+
+vector<vector<int> > ratInAMaze(vector<vector<int> > &maze, int n){
+  
+    vector <vector <int >> rat (n,vector <int> (n, 0));
+    vector <vector <int>> ans;
+    vector <vector <bool> > visited (n, vector <bool>(n, false));
+    solve (0, 0, maze, rat, ans, visited, n);
+    return ans;
+    
+}
