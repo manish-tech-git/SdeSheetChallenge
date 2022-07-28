427ad41d9890b457469e3d5f00f773f5e5b0aab8
Manish Kumar <95469979+manish-tech-git@users.noreply.github.com>
2022-07-28 21:13:50 +0530

    Create Palindrome Partitioning ll.cpp

---
+++ Palindrome Partitioning ll.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+bool isPalindrome (int i , int j, string str)
+{
+    while (i < j)
+    {
+        if (str[i] != str[j])  return false;
+            i++;
+            j--;
+           
+    }
+    return true;
+}
+
+int solve (int i, int n, string &str, vector <int> &dp)
+{
+    if ( i == n)
+          return 0;
+    if (dp[i] != -1)
+        return dp[i];
+    
+    int mini = INT_MAX;
+    
+    for (int j = i; j < n; j++){
+        if(isPalindrome(i, j, str)){
+            int cost = 1 + solve (j+1, n, str, dp );
+            mini = min (mini, cost);
+    }
+}
+    
+    return dp[i] = mini;
+}
+
+
+int palindromePartitioning(string str) {
+    
+    vector <int> dp (str.size(), -1);
+    return solve (0, str.size(), str, dp) - 1;
+ 
+}
