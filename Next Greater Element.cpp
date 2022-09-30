72a78edf3be1c38c036fb556e10c667a3263220b
Manish Kumar <95469979+manish-tech-git@users.noreply.github.com>
2022-09-30 09:57:13 +0530

    Update Next Greater Element.cpp

--- Next Greater Element.cpp
+++ Next Greater Element.cpp
@@ -8,30 +8,30 @@ ans ->  -1 6 -1 -1
 */
 
 
-#include <bits/stdc++.h>
-vector<int> nextGreater(vector<int> &arr, int n) {
+#include <bits/stdc++.h> 
+vector<int> nextGreaterElement(vector<int>& arr, int n)
+{
+    stack <int> st;
     
-    stack <int> s;
-    vector <int> v(n, -1); // Declaring vector with values -1
- 
+    st.push(-1);
+    vector <int> ans (n);
     
-    for (int i = arr.size() -1; i >= 0; i--){
-    
+    for (int i = n-1; i >= 0; i--)
+    {
+        int curr = arr[i];
         // if the current element is greater then top element of stack then we will pop the element of stack untill we find the smaller one.
-        while(!s.empty() && arr[i] >= s.top()){
-            s.pop();
-        }
-
-        if( i < n && !s.empty()) {
-            v[i] = s.top();
-        }
-    
-        s.push(arr[i]);
-    
+        while (!st.empty() && st.top() <= curr )
+            st.pop();
+        
+        if (st.empty())
+            ans[i] = -1;
+        else
+            ans[i] = st.top();
+        
+        st.push(curr);
+        
     }
-
-    return v;
-
+    return ans;
 }
 
 //Time Complexity: O(N)
